b2e94273242d45389cb3fa62594ee8d60c5b6feb
rieerep <114030611+rieerep@users.noreply.github.com>
2023-10-09 17:26:47 +0200

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using spa_project.Models;
+
+namespace spa_project.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public DbSet<GameModel> Games { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace spa_project.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Wins { get; set; }
+
+        public int Draws { get; set; }
+
+        public int losses { get; set; }
+        public int GamesPlayed { get; set; }
+        public virtual GameModel? Game { get; set; }
+
+    }
+}
