2f9f708a79d2fb7b3e5934c042f709ce9f64dde0
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-10-11 17:45:58 +0200

    Removed cstring.cs

--- CString.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SPAGame
-{
-    internal class CString
-    {
-        public static string connectionString = "Data Source=DN1L0S-D4TR; Initial Catalog=SpaGameProjectDB;Integrated Security=true";
-    }
-}
