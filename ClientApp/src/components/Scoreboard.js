d49ce117ada4e8e5b81e0d1349ba4d84619460c0
rieerep <114030611+rieerep@users.noreply.github.com>
2023-10-16 07:03:13 +0200

    wip highscore page

--- ClientApp/src/components/Scoreboard.js
+++ ClientApp/src/components/Scoreboard.js
@@ -1,8 +1,32 @@
-﻿import React from 'react';
+﻿import React, { useEffect } from 'react';
 
 const Scoreboard = () => {
+
+    const [scoreboard, setScoreboard] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/api/scoreboard');
+                const data = await response.json();
+                setScoreboard(data);
+            } catch (error) {
+                console.error('ERRRORR', error)
+            }
+        }
+
+        fetchData();
+    }, []);
+
     return (
-        <h1>Scoreboard</h1>
+        <div>
+            <h1>Scoreboard</h1>
+            <ul>
+                {scoreboard.map((score, index) => (
+                    <li key={index}>{score.name}: {score.score}</li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
